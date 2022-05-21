ed74198e49ee654850167cd86d83123b76fa5180
Nelson <nelsonfabianlotumolo@gmail.com>
2022-05-21 03:01:52 -0300

    Se crea funcion para modificar la altura del textarea

--- js/adaptar_txtarea.js
+++ js/adaptar_txtarea.js
@@ -1,11 +1,10 @@
-var txtArea = capturarElemento(".txt_entrada");
+var txtArea = document.querySelector(".txt_entrada");
 console.log(txtArea);
 
 txtArea.addEventListener("click", function() {
 	txtArea.textContent = "";
 });
 
-function capturarElemento(elemento) {
-	var aux = document.querySelector(elemento);
-	return aux;
-}
+txtArea.addEventListener("input", function() {
+	txtArea.style.height = "600px";
+});
